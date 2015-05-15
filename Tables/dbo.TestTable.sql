837a7de51bbd8a197b4a3f08f9f390d4a444b883
prasanthgv <prasanth_gv@amway.com>
2015-05-15 16:46:06 +0900

    Added new TestTable

---
+++ Tables/dbo.TestTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[TestTable]
+(
+[TestClm1] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[TestClm2] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[TestClm3] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
